c76fa0efc4f8b549ec80fe795c44eb04c1f4712f
Minhtri Tran <mtran7305@gmail.com>
2017-06-18 22:07:43 -0400

    Finish build process

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-// $(document).ready(function() {
-// 	$("#header").load("header.html", function() {
-// 		$('#nav-door-dropdown').hover(function() {
-// 			$(this).find('.dropdown-menu').stop(true, true).delay(150).fadeIn(100);
-// 		}, function() {
-// 			$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
-// 		});
-// 	});
-// });
+$(document).ready(function() {
+	$('#nav-door-dropdown').hover(function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(150).fadeIn(100);
+	}, function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
+	});
+});
 
